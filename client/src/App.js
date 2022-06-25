a3d2be194b773916b2fd17e3677477d144baf24d
Tanvirmehedi <tjmehedi201@gmail.com>
2022-06-26 01:51:05 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
